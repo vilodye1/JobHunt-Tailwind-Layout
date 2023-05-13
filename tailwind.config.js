d4a913a3cc96e2ffe53a89fa9f51fa3ffa372abb
vilodye1 <vilodye@gmail.com>
2023-05-13 13:53:31 -0400

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        platinum: 'hsl(222, 15%, 87%)',
+        powderBlue: 'hsl(211, 29%, 74%)',
+        moonstone: 'hsl(188, 36%, 52%)',
+        royalBlue: 'hsl(222, 82%, 21%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        charcoal: 'hsl(222, 19%, 27%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+        veryLightBlue: '#E9F4FF'
+      }
+    },
+  },
+  plugins: [],
+}
